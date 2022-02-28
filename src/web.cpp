9b990af3fac7bc26b81472762b65853fd8a66c6e
Mateusz <mwojciechowski296@gmail.com>
2022-02-28 12:57:47 +0100

    Udane połączenie c++ i JS

--- src/web.cpp
+++ src/web.cpp
@@ -15,12 +15,10 @@ using namespace std;
 
 extern "C" {
 
-EMSCRIPTEN_KEEPALIVE int getDistance8(const char *pattern, const char *word) {
-    auto distance = Levenshtein<unsigned short int>::getDistance(pattern, word);
-    printf(reinterpret_cast<const char *>(distance));
-    printf(pattern);
-    printf(word);
-    return distance;
+EMSCRIPTEN_KEEPALIVE int getDistance8(const char *pattern, const char *word, int patternLength, int wordLength) {
+    string patternStr(pattern, patternLength);
+    string wordStr(word, wordLength);
+    return Levenshtein<unsigned short int>::getDistance(patternStr, wordStr);
 }
 
 }
