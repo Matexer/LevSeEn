a0f78b807d45fbd6388c79b39024a8172242adc1
Mateusz <mwojciechowski296@gmail.com>
2022-02-28 11:22:00 +0100

    Prace nad webassembly

--- src/web.cpp
+++ src/web.cpp
@@ -13,12 +13,14 @@
 using namespace std;
 
 
-int main(int argc, char const *argv[])
-{
-    return Levenshtein<unsigned short int>::getDistance("eff", "fee");
+extern "C" {
+
+EMSCRIPTEN_KEEPALIVE int getDistance8(const char *pattern, const char *word) {
+    auto distance = Levenshtein<unsigned short int>::getDistance(pattern, word);
+    printf(reinterpret_cast<const char *>(distance));
+    printf(pattern);
+    printf(word);
+    return distance;
 }
 
-
-EMSCRIPTEN_KEEPALIVE unsigned short int getDistance8() {
-    return Levenshtein<unsigned short int>::getDistance("eff", "fee");
 }
