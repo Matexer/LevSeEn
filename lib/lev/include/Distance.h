56d81e04eb8cee0d571c1ba3e159792971e89153
Mateusz <mwojciechowski296@gmail.com>
2022-03-10 08:13:25 +0100

    Refactoring (wcięcia)

--- lib/lev/include/Distance.h
+++ lib/lev/include/Distance.h
@@ -8,60 +8,60 @@
 namespace Levenshtein {
 
 
-    template<typename SizeT>
-    class Distance {
-    public:
-        //  Konstruktor tworzący warunki do szybkiego porównywania słów o podanych długościach
-        Distance(const SizeT &patternLength, const SizeT &wordLength);
+template<typename SizeT>
+class Distance {
+public:
+    //  Konstruktor tworzący warunki do szybkiego porównywania słów o podanych długościach
+    Distance(const SizeT &patternLength, const SizeT &wordLength);
 
-        //  Konstruktor tworzący warunki do szybkiego porównywania słów o podanych długościach
-        //  oraz ustawiający nowe wartości kosztów edycji
-        Distance(const SizeT &patternLength, const SizeT &wordLength,
-                 SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
+    //  Konstruktor tworzący warunki do szybkiego porównywania słów o podanych długościach
+    //  oraz ustawiający nowe wartości kosztów edycji
+    Distance(const SizeT &patternLength, const SizeT &wordLength,
+             SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
 
-        Distance(const Distance& other);
-        Distance(Distance&& other) noexcept ;
+    Distance(const Distance& other);
+    Distance(Distance&& other) noexcept ;
 
-        // = działa jak przeniesienie z posprzątaniem obiektu przenoszonego (rozmontowywanego)
-        Distance& operator=(Distance &&other) {
-            this->stealAndDestroy(other);
-            return *this;
-        }
+    // = działa jak przeniesienie z posprzątaniem obiektu przenoszonego (rozmontowywanego)
+    Distance& operator=(Distance &&other) {
+        this->stealAndDestroy(other);
+        return *this;
+    }
 
-        //  Ustala koszty edycji dla niestatycznych funkcji składowych
-        void setEditCosts(SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
+    //  Ustala koszty edycji dla niestatycznych funkcji składowych
+    void setEditCosts(SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
 
-        //  Zwraca odległość Levenshteina, dla wzorca i słowa o ustalonej w konstruktorze długości
-        SizeT getDistance(const std::string &pattern, const std::string &word);
+    //  Zwraca odległość Levenshteina, dla wzorca i słowa o ustalonej w konstruktorze długości
+    SizeT getDistance(const std::string &pattern, const std::string &word);
 
-        //  Zwraca odległość edycji po samodzielnym sprawdzeniu długości wzorca i słowa
-        static SizeT getEditDistance(const std::string &pattern, const std::string &word);
+    //  Zwraca odległość edycji po samodzielnym sprawdzeniu długości wzorca i słowa
+    static SizeT getEditDistance(const std::string &pattern, const std::string &word);
 
-        //  Zwraca odległość Levenshteina po samodzielnym sprawdzeniu długości wzorca i słowa
-        static SizeT getDistance(const std::string &pattern, const std::string &second,
-                                 SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
+    //  Zwraca odległość Levenshteina po samodzielnym sprawdzeniu długości wzorca i słowa
+    static SizeT getDistance(const std::string &pattern, const std::string &second,
+                             SizeT deletionCost, SizeT insertionCost, SizeT swapCost);
 
-        ~Distance();
+    ~Distance();
 
-    protected:
-        static inline SizeT min(SizeT a, SizeT b, SizeT c);
+protected:
+    static inline SizeT min(SizeT a, SizeT b, SizeT c);
 
-        //Przypisuje sobie wszystkie wartości argumentów skałdowych
-        //zabiera tablice top i bot, dając swoje, a następnie sprząta
-        //rozmontowywany obiekt (usuwając je)
-        void stealAndDestroy(Distance& other);
+    //Przypisuje sobie wszystkie wartości argumentów skałdowych
+    //zabiera tablice top i bot, dając swoje, a następnie sprząta
+    //rozmontowywany obiekt (usuwając je)
+    void stealAndDestroy(Distance& other);
 
-        SizeT patternLength;
-        SizeT wordLength;
-        SizeT tableSize;
+    SizeT patternLength;
+    SizeT wordLength;
+    SizeT tableSize;
 
-        SizeT *top = nullptr;
-        SizeT *bot = nullptr;
+    SizeT *top = nullptr;
+    SizeT *bot = nullptr;
 
-        SizeT deletionCost = 1;
-        SizeT insertionCost = 1;
-        SizeT swapCost = 1;
-    };
+    SizeT deletionCost = 1;
+    SizeT insertionCost = 1;
+    SizeT swapCost = 1;
+};
 
 #ifdef UINT8_SizeT
     template class Distance<uint8_t>;
--- lib/lev/include/Search.h
+++ lib/lev/include/Search.h
@@ -9,54 +9,54 @@
 namespace Levenshtein {
 
 
-    template<typename SizeT>
-    class Search {
-    public:
-        static void setMultithreading(bool multithreading);
-        static void setMultithreadingMinComplexity(uint64_t multithreadingMinComplexity);
-        static void setDeletionCost(SizeT deletionCost);
-        static void setInsertionCost(SizeT insertionCost);
-        static void setSwapCost(SizeT swapCost);
+template<typename SizeT>
+class Search {
+public:
+    static void setMultithreading(bool multithreading);
+    static void setMultithreadingMinComplexity(uint64_t multithreadingMinComplexity);
+    static void setDeletionCost(SizeT deletionCost);
+    static void setInsertionCost(SizeT insertionCost);
+    static void setSwapCost(SizeT swapCost);
 
-        //Zwraca tablicę zawiarającą odległość dla każdego indexu w tekście
-        static std::vector<SizeT>* search(const std::string &pattern, const std::string &text);
+    //Zwraca tablicę zawiarającą odległość dla każdego indexu w tekście
+    static std::vector<SizeT>* search(const std::string &pattern, const std::string &text);
 
-    protected:
-        struct SearchData {
-            const std::string &pattern;
-            const std::string &text;
-            size_t firstIndex;
-            size_t lastIndex;
-            std::vector<SizeT>* const output;
-        };
-
-        static void search(SearchData &data);
-        static void _search(SearchData data);
-        static void concurrentSearch(SearchData &data);
-
-        static SizeT DELETION_COST;
-        static SizeT INSERTION_COST;
-        static SizeT SWAP_COST;
-
-        static bool MULTITHREADING;  // Zezwolenie na wielowątkowość
-        static uint64_t MULTITHREADING_MIN_COMPLEXITY;
+protected:
+    struct SearchData {
+        const std::string &pattern;
+        const std::string &text;
+        size_t firstIndex;
+        size_t lastIndex;
+        std::vector<SizeT>* const output;
     };
 
+    static void search(SearchData &data);
+    static void _search(SearchData data);
+    static void concurrentSearch(SearchData &data);
 
-    #ifdef UINT8_SizeT
-        template class Search<uint8_t>;
-    #endif
+    static SizeT DELETION_COST;
+    static SizeT INSERTION_COST;
+    static SizeT SWAP_COST;
 
-    #ifdef UINT16_SizeT
-        template class Search<uint16_t>;
-    #endif
+    static bool MULTITHREADING;  // Zezwolenie na wielowątkowość
+    static uint64_t MULTITHREADING_MIN_COMPLEXITY;
+};
 
-    #ifdef UINT32_SizeT
-        template class Search<uint32_t>;
-    #endif
 
-    #ifdef UINT64_SizeT
-        template class Search<uint64_t>;
-    #endif
+#ifdef UINT8_SizeT
+    template class Search<uint8_t>;
+#endif
+
+#ifdef UINT16_SizeT
+    template class Search<uint16_t>;
+#endif
+
+#ifdef UINT32_SizeT
+    template class Search<uint32_t>;
+#endif
+
+#ifdef UINT64_SizeT
+    template class Search<uint64_t>;
+#endif
 
 }
