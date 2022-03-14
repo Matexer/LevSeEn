198d349e5f322b986bae36dcab5c6f2e8ef97d9a
Mateusz <mwojciechowski296@gmail.com>
2022-03-14 08:41:30 +0100

    usunięto const

--- lib/lev/include/structs.h
+++ lib/lev/include/structs.h
@@ -4,11 +4,11 @@
 
 template<typename SizeT>
 struct SearchOutput {
-    const size_t index;
-    const SizeT distance;
+    size_t index;
+    SizeT distance;
 };
 
 template<typename SizeT>
 struct FixedSearchOutput : SearchOutput<SizeT> {
-    const SizeT length;
+    SizeT length;
 };
